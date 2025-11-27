9aa1f222f875f12a83a9b13436a96a449d56bd37
Paulo Henrique da Silva Mascarenhas <paulo.mascarenhas@ucsal.edu.br>
2025-11-27 15:21:48 -0300

    Rename ControleHistorico to HistoricoController

---
+++ src/modules/ads/HistoricoController.js
@@ -0,0 +1,12 @@
+export class HistoricoController {
+
+    buscarHistorico(idPaciente) {
+  
+        const historico = [
+            { data: "2025-10-10", medico: "Dr. Silva", status: "REALIZADA" },
+            { data: "2025-09-05", medico: "Dra. Ana", status: "FINALIZADA" }
+        ];
+
+        return historico;
+    }
+}
